c7f878220d30deb84fdf0529944d5f77a3329a3c
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-06 19:40:56 +0500

    first code

---
+++ 13. ARITHMETIC OPERATOR IN PYTHON.py
@@ -0,0 +1,21 @@
+
+print(5+4)
+print(3-2)
+print(25/5)
+print(4*2)
+print(3**2)
+print(2+3-4)
+print(2+3/2**2)
+print(4**2)
+
+
+print(3 + 4 * 4 / 10 * 20)
+
+#CALCULATOR
+first_number = float(input("Enter First Number: "))
+second_number = float(input("Enter Second Number: "))
+print(first_number + second_number)
+print(first_number - second_number)
+print(first_number / second_number)
+print(first_number * second_number)
+
